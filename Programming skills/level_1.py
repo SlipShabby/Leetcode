6e45a7c1632e009e0c33e2c9017b19270038bcaf
SlipShabby <sd.djaan@gmail.com>
2022-03-25 14:59:40 -0700

    Programming skills: Count Odd Numbers in an Interval Range

---
+++ Programming skills/level_1.py
@@ -0,0 +1,12 @@
+
+
+# Count Odd Numbers in an Interval Range 
+
+class Solution(object):
+    def countOdds(self, low, high):
+        if low % 2 == 0:
+            return (high-low+1)//2
+        return (high-low)//2 + 1
+            
+
+
