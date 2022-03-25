18d94af6689fb87151a4cd72be5eccc7f2a20ebe
SlipShabby <sd.djaan@gmail.com>
2022-03-25 15:13:35 -0700

    find average salary

--- Programming skills/level_1.py
+++ Programming skills/level_1.py
@@ -2,11 +2,25 @@
 
 # Count Odd Numbers in an Interval Range 
 
-class Solution(object):
+class SolutionOddNumber(object):
     def countOdds(self, low, high):
         if low % 2 == 0:
             return (high-low+1)//2
         return (high-low)//2 + 1
             
 
+# find average salary
 
+class SolutionAverageSalary(object):
+    def average(self, salary):
+       
+        total = 0
+        n = len(salary)
+        
+        for i in range(0, n):
+            total += salary[i]
+        
+        total = total-max(salary) - min(salary)
+
+            
+        return float(total)/float((n-2))
