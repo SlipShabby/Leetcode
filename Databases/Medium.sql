f0d97ef9302bcd5611a41ab1167dd4300ddf0494
SlipShabby <sd.djaan@gmail.com>
2022-03-28 10:50:22 -0700

    sql medium: find 2nd highest salary (using IFNULL)

--- Databases/Medium.sql
+++ Databases/Medium.sql
@@ -112,3 +112,17 @@ WHERE
         MAX(salary)
     FROM Employee e1
 );
+
+SELECT
+    IFNULL
+    (
+      (
+          SELECT 
+            DISTINCT Salary
+          FROM 
+            Employee
+          ORDER BY Salary DESC
+          LIMIT 1 OFFSET 1
+      ),
+    NULL
+    ) AS SecondHighestSalary;
