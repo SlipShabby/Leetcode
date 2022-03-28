f10cd437da25141bee287e75322b3b7589a32fa3
SlipShabby <sd.djaan@gmail.com>
2022-03-28 10:06:51 -0700

    find Consecutive Numbers: faster solution

--- Databases/Medium.sql
+++ Databases/Medium.sql
@@ -42,3 +42,23 @@ FROM
 WHERE
     num = next AND num = prior;
 
+SELECT
+    DISTINCT num AS ConsecutiveNums
+FROM
+    Logs
+WHERE
+    (id +1, num)
+    IN
+        (
+            SELECT *
+            FROM
+                Logs
+        )
+        AND (id+2, num)
+        IN
+            (
+                SELECT *
+                FROM
+                    Logs
+            );
+
