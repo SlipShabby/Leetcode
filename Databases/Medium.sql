6649dd73db3e2654c31c50a7bfef1fbdef630a09
SlipShabby <sd.djaan@gmail.com>
2022-03-29 16:37:06 -0700

    sql: swap the seat id

--- Databases/Medium.sql
+++ Databases/Medium.sql
@@ -160,3 +160,18 @@ FROM
 LEFT JOIN
     Tree t2
 ON t1.id = t2.p_id;
+
+
+# Swap seats
+
+SELECT
+	CASE
+		WHEN seat.id % 2 <> 0 AND seat.id = (SELECT COUNT(*) FROM seat) THEN seat.id
+		WHEN seat.id % 2 = 0 THEN seat.id - 1
+		ELSE
+			seat.id + 1
+	END as id,
+	student 
+FROM seat
+ORDER BY id
+;
